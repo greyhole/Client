c3ccd7d7e51db97c932554f2699bfbd55a68628e
Norman Vetter <sanve2@aol.com>
2013-06-13 12:18:50 +0200

    Start Commit

---
+++ Client1/src/Client1.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author nvetter
+ */
+public class Client1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame frame=new NewJFrame(false);
+        frame.setVisible(true);
+    }
+}
